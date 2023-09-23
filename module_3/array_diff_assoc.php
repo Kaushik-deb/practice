19da21a75d644baea5d3d23ea1246e753ffcf3cf
kaushik deb <kaushikdeb2k12kuet@gmail.com>
2023-09-24 00:14:10 +0600

    new

--- module_3/array_diff_assoc.php
+++ module_3/array_diff_assoc.php
@@ -6,14 +6,14 @@ $result=array_diff_assoc($a1,$a2);
 print_r($result);
 
 
-
-
-
-
-
-
-
-
+// Syntax
+// array_diff_assoc(array1,array2,array3...)
+// compares both key and values
+// Parameter Values
+// Parameter	Description
+// array1	Required. The array to compare from
+// array2	Required. An array to compare against
+// array3,...	Optional. More arrays to compare against
 
 
 ?>

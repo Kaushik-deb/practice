c16d3e05ad94d555c32179710c125c6bba801ddb
kaushik deb <kaushikdeb2k12kuet@gmail.com>
2023-09-23 22:57:52 +0600

    new

--- module_3/array_diff.php
+++ module_3/array_diff.php
@@ -4,4 +4,13 @@ $a2=array("e"=>"red","f"=>"green","g"=>"blue");
 
 $result=array_diff($a1,$a2);
 print_r($result);
+
+
+//syntax
+// array_diff(array1, array2, array3, ...)
+// Parameter	Description
+// array1	Required. The array to compare from
+// array2	Required. An array to compare against
+// array3,...	Optional. More arrays to compare against
+
 ?>
